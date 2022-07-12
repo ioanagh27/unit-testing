0a07a5bad1aa4b221180a1eee778f58d04ef63f6
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-07-12 10:47:53 +0100

    Group tests together

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,12 +1,17 @@
 const getLargest = require("../getLargest");
 
-test("getLargest is a function", () => {
-    expect(typeof getLargest).toBe("function");
+describe("getLargest", () => {
+    
+    test("getLargest is a function", () => {
+        expect(typeof getLargest).toBe("function");
+    })
+    
+    test("getLargest returns the largest value", () => {
+    
+        const result = getLargest(1, 2, 3);
+    
+        expect(result).toEqual(3);
+    })
 })
 
-test("getLargest returns the largest value", () => {
 
-    const result = getLargest(1, 2, 3);
-
-    expect(result).toEqual(3);
-})
